9785feabe32dc838c234d9c19b1d5976aee297b8
nakompuktere <144929164+nakompuktere@users.noreply.github.com>
2024-03-20 17:49:05 +0300

    publish photo to Telegram channel

--- python-telegram-bot.py
+++ python-telegram-bot.py
@@ -1,6 +1,8 @@
 import telegram
 
+
 chat_id = "@download_bott"
 bot = telegram.Bot(token='7145461467:AAG2NGgXg19dbyzC1w2jUe1yDkEIZkpt5T8')
 
 bot.send_message(chat_id=chat_id, text="hello")
+bot.send_document(chat_id=chat_id, document=open("images\spaceX_0.jpeg", 'rb'))
