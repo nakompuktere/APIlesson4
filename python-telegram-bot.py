8ec7769d0b2fb26ff356564b0b3dc20ced7fe15f
nakompuktere <144929164+nakompuktere@users.noreply.github.com>
2024-03-20 17:37:33 +0300

    publish text to Telegram channel.

--- python-telegram-bot.py
+++ python-telegram-bot.py
@@ -1,5 +1,6 @@
 import telegram
 
-
+chat_id = "@download_bott"
 bot = telegram.Bot(token='7145461467:AAG2NGgXg19dbyzC1w2jUe1yDkEIZkpt5T8')
-print(bot.get_me())
+
+bot.send_message(chat_id=chat_id, text="hello")
